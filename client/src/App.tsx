bd6b4b71a82b32bc3926d58966913ba898ca6e29
Harshalvk <harshalvkhobragade@gmail.com>
2024-02-17 11:17:05 +0530

    Add pages and routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,22 @@
+import {Routes, Route} from 'react-router-dom' 
+
+import Home from './Pages/Home'
+import Profile from './Pages/Profile'
+import About from './Pages/About'
+import SignIn from './Pages/SignIn'
+import SignOut from './Pages/SignOut'
 
 function App() {
 
   return (
     <>
-      <h1>Live</h1>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/sign-out' element={<SignOut/>}/>
+      </Routes>
     </>
   )
 }
