2c1eb16703c0707897bcfa3690ff7c4ffd6b0ffc
Harshalvk <harshalvkhobragade@gmail.com>
2024-02-18 16:04:26 +0530

    Add error handling

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,20 +1,35 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, Errback, NextFunction } from "express";
 import connectDB from "./db";
-import cors from 'cors'
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
-import userRouter from './routes/user.routes'
+import userRouter from "./routes/user.routes";
 
 const app = express();
 const PORT = process.env.PORT;
 
-connectDB()
+connectDB();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.use('/api/user', userRouter)
+app.use("/api/user", userRouter);
+
+type Error = {
+  statusCode: number | 500,
+  message: string | 'Internal server error'
+}
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  const statusCode = err.statusCode
+  const message = err.message 
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message
+  })
+});
 
 app.listen(PORT, () => {
   console.log(`⚙ Server is listening on http://localhost:${PORT}`);
