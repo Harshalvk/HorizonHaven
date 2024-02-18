9823e2f95662182a45fbf7ea0e0875c321e9f391
Harshalvk <harshalvkhobragade@gmail.com>
2024-02-18 10:29:01 +0530

    Add a utils function to handle possible errors

--- api/src/controller/user.controller.ts
+++ api/src/controller/user.controller.ts
@@ -1,22 +1,26 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import User from "../models/user.model";
-import bcrypt from 'bcryptjs'
+import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/errorHandler";
 
 export const test = (req: Request, res: Response) => {
   res.send({ msg: "Test route" });
 };
 
-export const signUp = async (req: Request, res: Response) => {
+export const signUp = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { username, email, password } = req.body;
-  const findUser = await User.findOne({email})
-  if(!findUser){
-    const hashedPassword = await bcrypt.hash(password, 10)
-    const newUser = new User({username, email, password: hashedPassword})
-    try {
-      await newUser.save()
-      res.send("User created successfully").status(201)
-    } catch (error: any) {
-      res.status(500).json({'🔴 Error while signup:': error.message})
-    }
+  const findUser = await User.findOne({ email });
+  if (!findUser) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
+
+    await newUser.save();
+    res.send("User created successfully").status(201);
+  } else {
+    next(errorHandler(500, 'User already exists'))
   }
 };
