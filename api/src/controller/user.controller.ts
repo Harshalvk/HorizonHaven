5c27dbf3f412a8c5dd573a811fcaed460e57a40c
Harshalvk <harshalvkhobragade@gmail.com>
2024-02-18 17:04:33 +0530

    User login functionality using JWT

--- api/src/controller/user.controller.ts
+++ api/src/controller/user.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import User from "../models/user.model";
 import bcrypt from "bcryptjs";
 import { errorHandler } from "../utils/errorHandler";
+import jwt from 'jsonwebtoken'
 
 export const test = (req: Request, res: Response) => {
   res.send({ msg: "Test route" });
@@ -12,9 +13,9 @@ export const signUp = async (
   res: Response,
   next: NextFunction
 ) => {
-  console.log(req.body.formData)
-  const { username, email, password } = req.body.formData
-  console.log(password)
+  console.log(req.body.formData);
+  const { username, email, password } = req.body.formData;
+  console.log(password);
   const findUser = await User.findOne({ email });
   if (!findUser) {
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -23,6 +24,30 @@ export const signUp = async (
     await newUser.save();
     res.send("User created successfully").status(201);
   } else {
-    next(errorHandler(500, 'User already exists'))
+    next(errorHandler(500, "User already exists"));
+  }
+};
+
+export const signIn = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.log(req.body);
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(400, "Invalid user credentials"));
+    const validPassword = await bcrypt.compare(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Invalid password!"));
+    const token = await jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const {password: pass, ...rest} = validUser._doc   
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+    errorHandler(401, "User does not exists!");
   }
 };
--- api/src/routes/user.routes.ts
+++ api/src/routes/user.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
-import { signUp, test } from "../controller/user.controller";
+import { signUp, test, signIn } from "../controller/user.controller";
 
 const route = Router();
 
 route.get("/test", test);
 route.post('/signup', signUp)
+route.post('/signin', signIn)
 
 export default route;
