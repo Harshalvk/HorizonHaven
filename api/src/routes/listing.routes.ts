0d194ac010265af9ac394c25e13cfc7ce077337d
Harshalvk <harshalvkhobragade@gmail.com>
2024-04-22 19:26:10 +0530

    Create Update listing api route

--- api/src/routes/listing.routes.ts
+++ api/src/routes/listing.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createListing, deleteListing } from "../controller/listing.controller";
+import { createListing, deleteListing, updateListing } from "../controller/listing.controller";
 import { verifyToken } from "../utils/verifyUser";
 
 const route = Router();
 
 route.post("/create", verifyToken, createListing);
 route.delete("/delete/:id", verifyToken, deleteListing);
+route.post("/update/:id", verifyToken, updateListing)
 
 export default route;
