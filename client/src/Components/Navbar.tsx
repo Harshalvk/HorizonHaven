817f82cfe24bf6798915754d07e71c5e54c0912b
Harshalvk <harshalvkhobragade@gmail.com>
2024-04-24 10:10:36 +0530

    Add header search query functionality

--- client/src/Components/Navbar.tsx
+++ client/src/Components/Navbar.tsx
@@ -1,9 +1,30 @@
+import React, { useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 
 function Navbar() {
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromURL = urlParams.get("searchTerm");
+    if(searchTermFromURL){
+      setSearchTerm(searchTermFromURL)
+    }
+  }, [location.search]);
+
   return (
     <header className=" bg-slate-200 shadow-sm">
       <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
@@ -11,13 +32,20 @@ function Navbar() {
           <span className="text-slate-900">Horizon</span>
           <span className="text-slate-500">Haven</span>
         </h1>
-        <form className="bg-slate-100 p-3 rounded-md flex items-center gap-3">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-100 p-3 rounded-md flex items-center gap-3"
+        >
           <input
             type="text"
             placeholder="Search"
             className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <FaSearch />
+          <button>
+            <FaSearch className="text-slate-700" />
+          </button>
         </form>
         <ul className="flex gap-2 items-center">
           <li className="hidden sm:inline font-semibold hover:underline">
