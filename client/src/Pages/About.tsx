ca4fbf051bbaa5f52c9e512876bef9cf5347509d
Harshalvk <harshalvkhobragade@gmail.com>
2024-04-26 22:09:22 +0530

    Add About page

--- client/src/Pages/About.tsx
+++ client/src/Pages/About.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <>
-      <h1>About page </h1>
-    </>
-  )
+    <div className="max-w-6xl mx-auto my-20 flex flex-col gap-3 font-semibold text-slate-600">
+      <h1 className="text-2xl font-bold sm:text-4xl text-slate-700">
+        About Horizon Haven
+      </h1>
+      <p className="text-justify">
+        Welcome to HorizonHaven, your gateway to seamless real estate
+        transactions and unparalleled property experiences. At HorizonHaven, we
+        believe that finding your dream property should be an adventure, not a
+        hassle. Whether you're buying, selling, or renting, we're here to guide
+        you every step of the way.
+      </p>
+      <p>
+        Our platform is designed with you in mind, offering a diverse range of
+        properties to suit every lifestyle and budget. From cozy apartments in
+        bustling urban centers to sprawling estates nestled in serene
+        countryside, HorizonHaven connects you with the perfect property,
+        wherever your dreams may take you.
+      </p>
+      <p>
+        What sets us apart is our commitment to excellence. We strive to provide
+        our users with the most comprehensive listings, cutting-edge technology,
+        and personalized support to ensure a smooth and rewarding experience.
+        Whether you're a first-time buyer or a seasoned investor, HorizonHaven
+        empowers you to make informed decisions and achieve your real estate
+        goals with confidence.
+      </p>
+      <p>
+        But HorizonHaven is more than just a marketplace – it's a community. We
+        believe in the power of connection and the importance of finding not
+        just a house, but a home. That's why we foster a vibrant community where
+        buyers, sellers, and renters come together to share their experiences,
+        insights, and aspirations. Join us on a journey to discover your
+        HorizonHaven – where your vision meets our expertise, and your dream
+        property becomes a reality. Welcome home.
+      </p>
+    </div>
+  );
 }
